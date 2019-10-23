a936ff97a4ceb841ebef837bd23a3cc4ad28aa4b
Pedro Amaral <pedro.amaral.eng@gmail.com>
2019-10-23 13:23:59 +0200

    Create session-init.sql

---
+++ utils/session-init.sql
@@ -0,0 +1,12 @@
+set echo on;
+--alter session set current_schema = <schema name>;
+alter session set nls_date_format='yyyy-mm-dd hh24:mi:ss';
+
+
+SET LINESIZE 600;
+SET PAGESIZE 1000;
+SET LONG 4000;
+--SPOOL 'D:\970317.log'
+
+
+
